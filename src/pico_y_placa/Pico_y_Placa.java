d0659f1167239d12e4b147dc265183e6be3ff1dd
Branre <garciabrandonarevalo@gmail.com>
2021-09-06 15:33:50 -0500

    Primer commit

---
+++ src/pico_y_placa/Pico_y_Placa.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pico_y_placa;
+
+
+/**
+ *
+ * @author brand
+ */
+public class Pico_y_Placa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+     
+    }
+   
+
+}
